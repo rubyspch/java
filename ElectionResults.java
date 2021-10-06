07efa6b988747447bcc5d3a68e1fef30748c5e22
rubyspch <ruby_chohan@hotmail.co.uk>
2021-10-06 13:50:29 +0100

    Add files via upload

---
+++ ElectionResults.java
@@ -0,0 +1,17 @@
+public class ElectionResults {
+
+    public void electionResults(){
+        float conservative = 100;
+        float labour = 60;
+        float green = 15;
+        float totalVotes = conservative + labour +green;
+        System.out.printf("The Conservative party got %.2f percent of the vote%n The Labour party got %.2f percent of the vote%n The Green party got %.2f percent of the vote%n", ((conservative/totalVotes)*100), ((labour/totalVotes)*100), ((green/totalVotes)*100));
+
+    }
+
+    public static void main(String[] args) {
+        Chapter2 go = new Chapter2();
+        go.electionResults();
+
+    }
+}
