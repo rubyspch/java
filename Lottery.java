cdfc697a35179a1911efb41bf0d5adb6cf1de6e9
rubyspch <ruby_chohan@hotmail.co.uk>
2021-10-14 19:02:52 +0100

    Add files via upload

--- Lottery.java
+++ Lottery.java
@@ -7,6 +7,7 @@ public class Lottery {
         int random2 = (int) (Math.random()*10);
         int random3 = (int) (Math.random()*10);
         int winnings=0;
+        int matches=0;
         Scanner scan = new Scanner(System.in);
         System.out.printf("Enter first digit%n");
         int first = scan.nextInt();
@@ -21,19 +22,37 @@ public class Lottery {
         if(userNum.equals(randomNum)){
             winnings=1000000;
         } else{
-            if(random1==first|| random1==second || random1==third){
-                if(random2==first|| random2==second || random2==third){
-                    if(random3==first|| random3==second || random3==third){
-                        winnings=1000;
-                    } else{
-                        winnings=100;
-                    }
+            if(first==random1||first==random2||first==random3){
+                matches+=1;
+            }
+            if(second==random1||second==random2||second==random3){
+                matches+=1;
+            }
+            if(third==random1||third==random2||third==random3){
+                matches+=1;
+            }
+            switch(matches){
+                case(1): {
+                    winnings = 10;
+                    break;
                 }
-            } else if(random2==first|| random2==second || random2==third && random3==first|| random3==second || random3==third){
-                winnings=100;
-            } else if(random2==first|| random2==second || random2==third && random3==first|| random3==second || random3==third){
+                case(2): {
+                    winnings = 100;
+                    break;
+                }
+                case(3): {
+                    winnings = 1000;
+                    break;
+                }
+                default:
+                    winnings=0;
             }
         }
+
+        System.out.printf("You guessed %d%d%d%n", first, second, third);
+        System.out.printf("Winning number was %d%d%d%n", random1,random2, random3);
+        System.out.printf("You have won %d%n", winnings);
+
     }
     public static void main(String[] args) {
         Lottery go = new Lottery();
