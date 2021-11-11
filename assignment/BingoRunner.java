95fd89787cd4a19153ceb84293576bd470c76f0a
rubyspch <ruby_chohan@hotmail.co.uk>
2021-11-11 21:38:53 +0000

    Add files via upload

---
+++ assignment/BingoRunner.java
@@ -0,0 +1,12 @@
+public class BingoRunner {
+  public static void main(String[] args) {
+    /* TODO
+          create and execute a new BingoController that starts the game
+          invoke run()
+          include the Thank you for playing once the game exits (GOODBYEMESSAGE)
+     */
+    BingoController start = new BingoController();
+    start.run();
+    System.out.printf(Toolkit.GOODBYEMESSAGE);
+  }
+}
