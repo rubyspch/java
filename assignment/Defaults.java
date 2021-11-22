02b3bbb32a2fa7f5596787b5a09527d1d248142d
rubyspch <ruby_chohan@hotmail.co.uk>
2021-11-22 23:40:41 +0000

    Add files via upload

--- assignment/Defaults.java
+++ assignment/Defaults.java
@@ -3,19 +3,19 @@
  */
 public class Defaults {
 
-  public static final int DEFAULT_NUMBER_OF_ROWS    = 4;
-  public static final int DEFAULT_NUMBER_OF_COLUMNS = 4;
+    public static final int DEFAULT_NUMBER_OF_ROWS = 4;
+    public static final int DEFAULT_NUMBER_OF_COLUMNS = 4;
 
-  public static final int NO_WINNER = -1;
+    public static final int NO_WINNER = -1;
 
-  private static String numberSeparator = " ";
+    private static String numberSeparator = " ";
 
-  public static String getNumberSeparator() {
-    return numberSeparator;
-  }
+    public static String getNumberSeparator() {
+        return numberSeparator;
+    }
 
-  public static void setNumberSeparator(String separator) {
-    numberSeparator = separator;
-  }
+    public static void setNumberSeparator(String separator) {
+        numberSeparator = separator;
+    }
 
 }
