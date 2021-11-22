0400aab295f5747c22b92e60d28814a5e1755e8e
rubyspch <ruby_chohan@hotmail.co.uk>
2021-11-22 23:40:56 +0000

    Add files via upload

--- assignment/Toolkit.java
+++ assignment/Toolkit.java
@@ -16,12 +16,12 @@ public class Toolkit {
     public static String printArray(String[] array) {
         StringBuilder sb = new StringBuilder("You entered\n");
         /* TODO
-        create a loop to print the numbers out once a user has inputted the BingoCard numbers, separated by commas (trim leading / trailing spaces)
-        check the expected output here to ensure that it appears as it should
-        return as a sb.toString()
-   */
+            create a loop to print the numbers out once a user has inputted the BingoCard numbers, separated by commas (trim leading / trailing spaces)
+            check the expected output here to ensure that it appears as it should
+            return as a sb.toString()
+        */
         for (int i = 0; i < array.length; ++i) {
-            sb.append(array[i]);
+            sb.append(array[i].trim());
             if (i < array.length - 1)
                 sb.append(", ");
         }
