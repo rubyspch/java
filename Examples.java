65449d30fa27b8621f998d1ce969ba41403db0a9
rubyspch <ruby_chohan@hotmail.co.uk>
2021-12-11 18:38:28 +0000

    Add files via upload

---
+++ Examples.java
@@ -0,0 +1,11 @@
+public class Examples {
+    public static void main(String[] args) {
+        try{
+            int[] a = {4,5,1};
+            System.out.printf(a[0]+"");
+        } catch(Exception e){
+            System.out.printf("An exception!");
+        }
+
+    }
+}
