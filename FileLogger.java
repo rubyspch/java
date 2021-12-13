2cc66a5ff3f6f1fc7bacda2e71970a14ed8ff5b6
rubyspch <ruby_chohan@hotmail.co.uk>
2021-12-13 18:27:51 +0000

    Add files via upload

--- FileLogger.java
+++ FileLogger.java
@@ -3,24 +3,40 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileLogger implements Logger {
-    private static final String FILE_LOGGER_NAME =  "StudentFileOutput.txt";
+    private static final String FILE_LOGGER_NAME = "StudentFileOutput.txt";
 
     static {
 
-        /** TODO
+        /* TODO
          * create a new File object for FILE_LOGGER_NAME
          * if the file already exists, delete it first
          * use try/catch block
          */
+        File fileObj = new File(FILE_LOGGER_NAME);
+        if (fileObj.exists())
+            fileObj.delete();
+//        else {
+//            try {
+//                fileObj.createNewFile();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     @Override
-    public void log (String message) {
-        /** TODO
-         * create a new FileWriter in append mode
-         * write the message to file
-         * check the ExpectedOutput files
-         * use try-with-resources/catch block
+    public void log(String message) {
+        /* TODO
+          create a new FileWriter in append mode
+          write the message to file
+          check the ExpectedOutput files
+          use try-with-resources/catch block
          */
+        try (FileWriter writer = new FileWriter(FILE_LOGGER_NAME, true)){
+            writer.write(message);
+        } catch (IOException e) {
+            e.printStackTrace(); //not sure this is the correct catch?
+        }
+
     }
 }
