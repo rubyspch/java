bf141dea696bcf0a3de84a5895176ec245b362c7
rubyspch <ruby_chohan@hotmail.co.uk>
2021-12-15 00:03:32 +0000

    Add files via upload

--- Mapping.java
+++ Mapping.java
@@ -13,7 +13,7 @@ public class Mapping {
 
     /* TODO
      * create a vocabulary HashMap to store all directions a user can go
-     */
+    */
     HashMap<String, String> vocabMapping = new HashMap<>();
 
     /* TODO
@@ -21,8 +21,7 @@ public class Mapping {
      */
     FileLogger fileLoggerObj = new FileLogger();
 
-    /*
-     * TODO
+    /*TODO
      * create a ConsoleLogger object
      */
     ConsoleLogger consoleLoggerObj = new ConsoleLogger();
@@ -45,16 +44,13 @@ public class Mapping {
         vocabMapping.put("UP", "U");
         vocabMapping.put("DOWN", "D");
         vocabMapping.put("EAST", "E");
-        //Add all of the possible variations from the output files to a hashmap.
     }
 
     public void mapping() {
-
         /* TODO
          * create a Scanner object
          */
         Scanner sc = new Scanner(System.in);
-
         /*
          * initialise a location variable with the INITIAL_LOCATION
          */
@@ -66,6 +62,7 @@ public class Mapping {
         String[] inputArray;
         boolean acceptedDirection = false;
         Location currentPlace;
+        String currentElement;
 
         // change this to be while location is not 0?
         while (location != 0) {
@@ -80,20 +77,16 @@ public class Mapping {
             /* TODO
              * verify if the location is exit
              */
-            //check if its 0
             if (currentPlace.getLocationId() != 0) {
                 /* TODO
                  * get a map of the exits for the location
                  */
-                //get the exits from Location.exits() (I think from there maybe elsewhere)
                 locationExits = currentPlace.getExits();
                 /* TODO
                  * print the available exits (to both console and file)
                  * crosscheck with the ExpectedOutput files
                  * Hint: you can use a StringBuilder to append the exits
                  */
-                //print the "Available exits are" line, possibly using string builder
-                // "Available exits are Q, E, W, U, SW, D, S, NW, SE, NE, N, "
                 message = new StringBuilder("Available exits are ");
                 for (String i : locationExits.keySet()) {
                     message.append(i).append(", ");
@@ -105,8 +98,7 @@ public class Mapping {
                  * input a direction
                  * ensure that the input is converted to uppercase
                  */
-                //get scanner input, nextLine()
-                input = sc.nextLine();
+                input = sc.nextLine().toUpperCase();
                 /* TODO
                  * are we dealing with a letter / word for the direction to go to?
                  * available inputs are: a letter(the HashMap value), a word (the HashMap key), a string of words that contains the key
@@ -120,20 +112,17 @@ public class Mapping {
                  * otherwise print an error message (to both console and file)
                  * check the ExpectedOutput files
                  */
-                //check the input has a direction in it, will need to split and trim the input I think - check the expected outputs
-                String currentElement;
                 inputArray = input.split(" ");
-                if (inputArray.length > 1 || inputArray[0].equalsIgnoreCase("UP") || inputArray[0].length()>2) {
-                    for (int i = 0; i < inputArray.length; i++) {
-                        currentElement = inputArray[i].toUpperCase();
+                if (inputArray.length > 1 || inputArray[0].length()>2 || inputArray[0].equalsIgnoreCase("UP")) {
+                    for (String s : inputArray) {
+                        currentElement = s;
                         if (vocabMapping.containsKey(currentElement) && locationExits.containsKey(vocabMapping.get(currentElement))) {
-                            //chosenDirection = vocabMapping.get(currentElement);
                             location = locationExits.get(vocabMapping.get(currentElement));
                             acceptedDirection = true;
                         }
                     }
                 } else {
-                    currentElement = inputArray[0].toUpperCase();
+                    currentElement = inputArray[0];
                     if (vocabMapping.containsValue(currentElement) && locationExits.containsKey(currentElement)) {
                         location = locationExits.get(currentElement);
                         acceptedDirection = true;
@@ -146,8 +135,6 @@ public class Mapping {
                 acceptedDirection = false;
             }
 
-            //either change location or do the error process in the outputs
-            //look up the integer key in location map, grab the location value, check the available exits
         }
         currentPlace = locationMap.get(0);
         fileLoggerObj.log(currentPlace.getDescription() + "\n");
