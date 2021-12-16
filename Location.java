12aeb0a6c3055e3f4a60954f17ff0b13ae0b1d66
rubyspch <ruby_chohan@hotmail.co.uk>
2021-12-16 02:44:42 +0000

    Add files via upload

--- Location.java
+++ Location.java
@@ -3,49 +3,62 @@ import java.util.Map;
 
 public class Location {
 
-    /** TODO
+    /**
+     * TODO
      * declare private final locationId, description, exits
      */
+    private final int locationId;
+    private final String description;
+    private final LinkedHashMap<String, Integer> exits;
 
 
-//constructor
+    //constructor
     public Location(int locationId, String description, Map<String, Integer> exits) {
-        /** TODO
+        /* TODO
          * set the locationId and the description
          */
+        this.locationId = locationId;
+        this.description = description;
 
-        /** TODO
+        /* TODO
          * if exits are not null, set the exit
          * Note that exits should be of type LinkedHashMap to maintain the insertion order
          * otherwise, set the exits LinkedHashMap to (Q,0)
          */
+        if (exits != null)
+            this.exits = new LinkedHashMap<>(exits);
+        else {
+            this.exits = new LinkedHashMap<>();
+            this.exits.put("Q", 0);
+        }
     }
 
     protected void addExit(String direction, int location) {
-        /** TODO
+        /* TODO
          * put the direction and the location in the exits LinkedHashMap
          */
+        this.exits.put(direction, location);
     }
 
     public int getLocationId() {
-        /** TODO
+        /* TODO
          * complete getter to return the location id
          */
-        return 0;
+        return this.locationId;
     }
 
     public String getDescription() {
-        /** TODO
+        /* TODO
          * complete getter to return the description
          */
-        return null;
+        return this.description;
     }
 
     public Map<String, Integer> getExits() {
-        /** TODO
+        /* TODO
          * complete getter to return a copy of exits
          * (preventing modification of exits from outside the Location instance)
          */
-        return null;
+        return this.exits;
     }
 }
